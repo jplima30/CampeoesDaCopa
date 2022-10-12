405d9e3ca3f1de80fdc3b59a266aff32df2b9db1
jplima <jplimadev@gmail.com>
2022-10-12 17:20:42 -0300

    ScreenSettingsDone

--- CampeoesDaCopa/CampeoesDaCopa/Controllers/WorldCupViewController.swift
+++ CampeoesDaCopa/CampeoesDaCopa/Controllers/WorldCupViewController.swift
@@ -4,21 +4,53 @@ class WorldCupViewController: UIViewController {
 
     var worldCup: WorldCup!
     
+    @IBOutlet weak var ImageViewWinner: UIImageView!
+    @IBOutlet weak var imageViewVice: UIImageView!
+    @IBOutlet weak var labelScore: UILabel!
+    @IBOutlet weak var labelWinner: UILabel!
+    @IBOutlet weak var labelVice: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        print("Ano da copa selecionado: \(worldCup.year)")
+        title = "WorldCup \(worldCup.year)"
+        ImageViewWinner.image = UIImage(named:"\(worldCup.winner).png")
+        imageViewVice.image = UIImage(named:"\(worldCup.vice).png")
+        labelWinner.text = worldCup.winner
+        labelVice.text = worldCup.vice
+        labelScore.text = "\(worldCup.winnerScore) * \(worldCup.viceScore)"
+    }
+}
+
+extension WorldCupViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return worldCup.matches.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let games = worldCup.matches[section].games
+        return games.count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! GamesTableViewCell
+        
+        let match = worldCup.matches[indexPath.section]
+        let game = match.games[indexPath.row]
+        
+        cell.prepare(with: game)
+        
+        return cell
+    }
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let match = worldCup.matches[section]
+        return match.stage
+    }
+    
+}
 
+extension WorldCupViewController: UITableViewDelegate {
+    
 }
